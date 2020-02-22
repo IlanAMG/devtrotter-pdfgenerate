70f97734c1d822ac952f33779a13f70176f16749
Ilan Amzallag <Ilan@MacBook-Air-de-Amzallag.local>
2020-02-22 19:42:26 +0100

    pdfgenerator-v1.0.1

--- src/components/Infos/Infos.js
+++ src/components/Infos/Infos.js
@@ -7,8 +7,8 @@ export const Infos = ({ showFact, showForm, handleShowDevisForm, handleShowFactF
             <h1>PDF Generator</h1> 
             <div className='container-choix'>
                 <button onClick={handleShowDevisForm}>Créer un nouveau Devis</button>
-                {showFact ? <h2 style={{alignSelf: 'center', color: '#fff' ,margin: 0, height: '40px'}}>Nouvelle Facture</h2> : null}
-                {showForm ? <h2 style={{alignSelf: 'center', color: '#fff',margin: 0, height: '40px'}}>Nouveau Devis</h2> : null}
+                {showFact ? <h2 style={{fontSize: '2em', alignSelf: 'center', color: '#fff' ,margin: 0, height: '40px'}}>Nouvelle Facture</h2> : null}
+                {showForm ? <h2 style={{fontSize: '2em', alignSelf: 'center', color: '#fff',margin: 0, height: '40px'}}>Nouveau Devis</h2> : null}
                 <button onClick={handleShowFactForm}>Créer une nouvelle Facture</button>
             </div>
         </>
